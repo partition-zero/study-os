09e8c6d49a4ed951457a7175cd11560820e936d0
partition-zero <2a3n0k1u0r9@gmail.com>
2026-08-12 18:50:14 +0530

    fixing some errors...

--- student_form/models.py
+++ student_form/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class StudentState(models.Model):
     s_name = models.CharField(default='Student',max_length = 255,error_messages='Enter Your Name Again')
-    s_topics = models.CharField(null=True,Blank=True,default='none',max_length = 255)
+    s_topics = models.CharField(null=True,blank=True,default='none',max_length = 255)
     s_mastery = models.FloatField(null=True,blank=True,verbose_name='Mastery')
     s_av_time = models.IntegerField(help_text='Enter Available Time to Study per day(hours)',
         error_messages='Enter Available Time to Study per day(hours)')
